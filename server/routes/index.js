28f7f4317cf0402de6a3e43f69f76e18de9c04ad
Victoria Cabral <43216482+toQhom@users.noreply.github.com>
2021-09-13 21:20:30 -0400

    Task 2: Added a line in /routes/index.js to create a /states route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+const { nextTick } = require('process');
 
 // Load data, parse to json
 const data = fs.readFileSync('./data/states.json');
 const states = JSON.parse(data);
 
 // TODO: Create a GET '/states' route that returns the JSON list states constant
+router.get('/states', function (req, res) {
+    res.send(states);
+    console.log('sent states');
+});
 
 module.exports = router;
